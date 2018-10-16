ce41b616cc714bcb163b91f4d09dc2d8468e7083
TerkaSlaninakova <xslanin@fi.muni.cz>
2018-10-16 08:50:11 +0200

    Added getters and setters for UserEntity

--- lost-and-found-service-persistence/src/main/java/cz/muni/fi/Entity/UserEntity.java
+++ lost-and-found-service-persistence/src/main/java/cz/muni/fi/Entity/UserEntity.java
@@ -9,4 +9,52 @@ public class UserEntity {
     private String name;
     private String email;
     private boolean isAdmin;
+
+    /**
+     *
+     * @return name of the user
+     */
+    public String getName(){
+        return name;
+    }
+
+    /**
+     *
+     * @param name of the user to set
+     */
+    public void setName(String name){
+        this.name = name;
+    }
+
+    /**
+     *
+     * @return email of the user
+     */
+    public String getEmail(){
+        return email;
+    }
+
+    /**
+     *
+     * @param email of the user to set
+     */
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    /**
+     *
+     * @return isAdmin property of user
+     */
+    public Boolean getIsAdmin(){
+        return isAdmin;
+    }
+
+    /**
+     *
+     * @param isAdmin property of the user to set
+     */
+    public void setIsAdmin(Boolean isAdmin){
+        this.isAdmin = isAdmin;
+    }
 }
