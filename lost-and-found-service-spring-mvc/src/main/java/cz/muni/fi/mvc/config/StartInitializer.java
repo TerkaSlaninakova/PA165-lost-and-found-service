40b6d4524e32bd3e70d6e11a8848cc352e3f3a7e
jakubpolacek <jkb.polacek@gmail.com>
2018-12-15 15:13:33 +0100

    kostra ale nieco nefunguje

---
+++ lost-and-found-service-spring-mvc/src/main/java/cz/muni/fi/mvc/config/StartInitializer.java
@@ -0,0 +1,37 @@
+package cz.muni.fi.mvc.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+/**
+ * Replaces web.xml file.
+ *
+ * @author Jakub Polacek
+ */
+public class StartInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{MySpringMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("utf-8");
+        return new Filter[]{encodingFilter};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return null;
+    }
+
+}
---
+++ lost-and-found-service-spring-mvc/src/main/java/cz/muni/fi/mvc/controllers/CategoryController.java
@@ -0,0 +1,82 @@
+package cz.muni.fi.mvc.controllers;
+
+import cz.muni.fi.dto.CategoryCreateDTO;
+import cz.muni.fi.facade.CategoryFacade;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.validation.Valid;
+
+/**
+ * SpringMVC Controller for administering categories.
+ *
+ * @author Martin Kuba makub@ics.muni.cz
+ */
+@Controller
+@RequestMapping("/category")
+public class CategoryController {
+
+    final static Logger log = LoggerFactory.getLogger(CategoryController.class);
+
+
+    @Autowired
+    private CategoryFacade categoryFacade;
+
+    /**
+     * Shows a list of products with the ability to add, delete or edit.
+     *
+     * @param model data to display
+     * @return JSP page name
+     */
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        model.addAttribute("categories", categoryFacade.getAllCategories());
+        return "category/list";
+    }
+
+    /**
+     * Prepares an empty form.
+     *
+     * @param model data to be displayed
+     * @return JSP page
+     */
+    @RequestMapping(value = "/new", method = RequestMethod.GET)
+    public String newCategory(Model model) {
+        log.debug("new()");
+        model.addAttribute("categoryCreate", new CategoryCreateDTO());
+        return "category/new";
+    }
+
+    @RequestMapping(value = "/create", method = RequestMethod.POST)
+    public String create(@Valid @ModelAttribute("categoryCreate") CategoryCreateDTO formBean, BindingResult bindingResult,
+                         Model model, RedirectAttributes redirectAttributes, UriComponentsBuilder uriBuilder) {
+        log.debug("create(formBean={})", formBean);
+        //in case of validation error forward back to the the form
+        if (bindingResult.hasErrors()) {
+            for (ObjectError ge : bindingResult.getGlobalErrors()) {
+                log.trace("ObjectError: {}", ge);
+            }
+            for (FieldError fe : bindingResult.getFieldErrors()) {
+                model.addAttribute(fe.getField() + "_error", true);
+                log.trace("FieldError: {}", fe);
+            }
+            return "category/new";
+        }
+        //create product
+        categoryFacade.addCategory(formBean);
+        //report success
+        redirectAttributes.addFlashAttribute("alert_success", "Category was created");
+        return "redirect:" + uriBuilder.path("/category/list").build().toUriString();
+    }
+}
